732189e7cd293a0d24feaf7861942836477612e4
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-12-19 15:16:04 -0500

    commit tree 5783cd35eaec8bd1e1c580535a40d472d5ee286f

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,4 @@
+class CostumeStore < ActiveRecord::Base
+end
+
 # Create your CostumeStore class here
--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,3 +1,19 @@
+class CreateCostumes < ActiveRecord::Migration[5.1]
+    def change
+        create_table :costumes do |t|
+            t.string :name
+            t.float :price
+            t.string :size
+            t.string :image_url
+            t.datetime :created_at
+            t.datetime :updated_at
+        end
+    end
+end
+
+
+
+
 # Create a class and inherit from ActiveRecord::Migration
 
 # By convention, the class name should match the part of the
